720d80eec3f6e8e23416b9f6770888adf6de49c7
Kwun Yu (Kaden) Wu <kw2963@nyu.edu>
2024-02-12 22:21:38 -0500

    Finished Assignment 2!

--- analyze.py
+++ analyze.py
@@ -1,1 +1,40 @@
 # Place code below to do the analysis part of the assignment.
+import csv
+import os
+
+#Opens the csv with all the data
+platform_agnostic_file_path_analyze = os.path.join('data', 'cleaned_data.csv')
+f = open(platform_agnostic_file_path_analyze, "r")
+readData = csv.DictReader(f)
+
+
+keys = ["Jan", "Feb", "Mar", "Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+
+localSum = 0
+yearCounter = 0
+
+
+#The loop works as follows:
+#The localSum and yearCounter varaibles will keep track the temporary sum and number of years summed over. These values will be reset to 0 at the end of every decade.
+#We will continue to sum up (by every row) until we get to any year with the last digit 9, where we will output the average.
+
+for line in readData:
+    for i in range(0, len(keys)):
+        localSum += float(line[keys[i]])
+    if int(line["Year"][3]) == 9:
+        if int(line["Year"]) - 9 == 1880:
+            print(f"The Average temperature change anomalies in Farenheit between 1881 and 1899 is {round(localSum/(yearCounter*12),2)} degrees")
+        else:
+            print(f"The Average temperature change anomalies in Farenheit between {int(line['Year']) - 9} and {line['Year']} is {round(localSum/(yearCounter*12), 2)} degress")
+        localSum = 0
+        yearCounter = 0
+    
+    finalYear = line['Year']
+    yearCounter += 1
+
+#This section is for the remaining years (where the last digit is less than 9)
+#The average for the remaining years that don't make up a decade
+remainingYearCounter = yearCounter - 1
+print(f"The Average temperature change anomalies in Farenheit between {int(finalYear) - remainingYearCounter + 1} and {int(finalYear)} is {round(localSum/(remainingYearCounter*12), 2)} degress")
+
+
--- munge.py
+++ munge.py
@@ -1,1 +1,66 @@
 # Place code below to do the munging part of this assignment.
+import os
+platform_agnostic_file_path = os.path.join('data', 'NasaData.txt')
+
+
+#This opens the file in read mode
+f = open(platform_agnostic_file_path, 'r')
+allLines = f.readlines()
+
+#Removes the heading in the file
+for i in range(0, 7):
+    del allLines [0]
+
+    length = len(allLines)
+    del allLines[length - 1]
+
+
+#Removes all the line breaks and the extra headings
+j = 1
+while j < len(allLines):
+    if allLines[j] == "\n":
+        allLines.pop(j)
+        j -= 1
+    elif allLines[j][:4] == "Year":
+        allLines.pop(j)
+        j -= 1
+    j += 1
+
+
+#This is where I selected to delete all rows of data that have missing data. As long as they are indicated by at least one *, then that whole row is deleted.
+k = 1
+while k < len(allLines):
+    temp = allLines[k]
+    if ("*" in temp):
+        allLines.pop(k)
+        k-= 1
+    k+=1
+
+
+data = []
+#This removes the last year index and changes all the values into %change in F
+#This is stored in data
+for k in range(0, len(allLines)):
+    tempData = allLines[k].strip().split()[:19]
+    if k >= 1:
+        for kp in range (1, len(tempData)):
+            tempData[kp] = format(((float(tempData[kp])/100) * 1.8), '.1f')
+    data += [tempData]
+
+#Creates and open a new file that will store all of the data in csv format
+#The new file is called cleanedData.csv
+newFile = os.path.join('data', 'cleaned_data.csv')
+
+newF = open(newFile, "w")
+
+#Stores the rows of values as comma separated values
+for pl in range(0, len(data)):
+    row = data[pl]
+    line = ""
+    for val in row:
+        line += val + ','
+    line = line[:-1]
+    if(pl == len(data) - 1):
+        newF.write(line)
+    else:
+        newF.write(line + '\n')
